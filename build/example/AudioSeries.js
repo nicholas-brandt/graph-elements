1ac636e2c56453443b59a1a14cbc5332d93930c6
Nicholas-Philip Brandt <nicholas.brandt@mail.de>
2018-08-21 10:13:56 +0000

    Last audio experiment

---
+++ build/example/AudioSeries.js
@@ -0,0 +1,24 @@
+export default class AudioSeries extends Array {
+    constructor(max_length, ...args) {
+        super(...args);
+        this.maxLength = max_length;
+        this.__sum = 0;
+        this.__squareSum = 0;
+    }
+    add(value) {
+        this.push(value);
+        this.__sum += value;
+        this.__squareSum += value ** 2;
+        if (this.length > this.maxLength) {
+            const value_removed = this.shift();
+            this.__sum -= value_removed;
+            this.__squareSum -= value_removed ** 2;
+        }
+    }
+    get deviation() {
+        return Math.sqrt((this.__squareSum - this.__sum ** 2 / this.length) / (this.length - 1));
+    }
+    get average() {
+        return this.__sum / this.length;
+    }
+}

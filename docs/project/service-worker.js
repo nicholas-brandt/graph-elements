2d5df05fab3ae850aa063f519a890a7b10ad0c7f
Nicholas-Philip Brandt <nicholas.brandt@mail.de>
2021-01-14 14:12:09 +0000

    service worker

---
+++ docs/project/service-worker.js
@@ -0,0 +1,35 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+workbox.routing.registerRoute(/^https:\/\/opencitations\.net/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'open-citations',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+      })
+    ]
+  }),
+);
+
+workbox.routing.registerRoute(/^https:\/\/(cdn\.jsdelivr\.net|hammerjs\.github\.io|rawgit\.com|d3js\.org|dev\.jspm\.io)/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'prerequisites',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+      })
+    ]
+  }),
+);
+
+workbox.routing.registerRoute(/^https:\/\/geneva-avenue\.codio\.io/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'codio',
+  }),
+);
+
+workbox.routing.registerRoute(/^https:\/\/jamtis\\.github\.io/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'codio',
+  }),
+);

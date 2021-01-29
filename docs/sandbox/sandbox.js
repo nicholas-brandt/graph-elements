944df925af3230c95aed09a58171a0ac379f6195
Nicholas-Philip Brandt <nicholas.brandt@mail.de>
2021-01-29 15:37:21 +0100

    Update sandbox.js

--- docs/sandbox/sandbox.js
+++ docs/sandbox/sandbox.js
@@ -11,13 +11,13 @@ import "//dev.jspm.io/@polymer/paper-button/paper-button.js";
 import "//dev.jspm.io/@polymer/paper-listbox/paper-listbox.js";
 import "//dev.jspm.io/@polymer/polymer/lib/elements/dom-repeat.js";
 
-import GraphDetailView from "//cdn.jsdelivr.net/gh/jamtis/graph-elements/build/elements/graph-detail-view/graph-detail-view.js";
-import GraphModifier from "//cdn.jsdelivr.net/gh/jamtis/graph-elements/build/elements/graph-modifier/graph-modifier.js";
-import GraphTracker from "//cdn.jsdelivr.net/gh/jamtis/graph-elements/build/elements/graph-tracker/graph-tracker.js";
-import GraphD3Force from "//cdn.jsdelivr.net/gh/jamtis/graph-elements/build/elements/graph-d3-force/graph-d3-force.js";
-import "//cdn.jsdelivr.net/gh/jamtis/graph-elements/build/elements/graph-d3-force/extensions/contextmenu.js";
-import GraphContextmenu from "//cdn.jsdelivr.net/gh/jamtis/graph-elements/build/elements/graph-contextmenu/graph-contextmenu.js";
-import GraphDisplay from "//cdn.jsdelivr.net/gh/jamtis/graph-elements/build/elements/graph-display/graph-display.js";
+import GraphDetailView from "//cdn.jsdelivr.net/gh/jamtis/graph-elements@master/build/elements/graph-detail-view/graph-detail-view.js";
+import GraphModifier from "//cdn.jsdelivr.net/gh/jamtis/graph-elements@master/build/elements/graph-modifier/graph-modifier.js";
+import GraphTracker from "//cdn.jsdelivr.net/gh/jamtis/graph-elements@master/build/elements/graph-tracker/graph-tracker.js";
+import GraphD3Force from "//cdn.jsdelivr.net/gh/jamtis/graph-elements@master/build/elements/graph-d3-force/graph-d3-force.js";
+import "//cdn.jsdelivr.net/gh/jamtis/graph-elements/build/elements@master/graph-d3-force/extensions/contextmenu.js";
+import GraphContextmenu from "//cdn.jsdelivr.net/gh/jamtis/graph-elements@master/build/elements/graph-contextmenu/graph-contextmenu.js";
+import GraphDisplay from "//cdn.jsdelivr.net/gh/jamtis/graph-elements@master/build/elements/graph-display/graph-display.js";
 
 import requestAnimationFunction from "//cdn.jsdelivr.net/npm/requestanimationfunction/requestAnimationFunction.js";
 

7411205437558cb4a1412acf4b123c6a2c2e6fa9
Nicholas-Philip Brandt <nicholas.brandt@mail.de>
2021-01-29 14:44:21 +0000

    build graph-adph force

--- build/elements/graph-d3-force/graph-d3-force.js
+++ build/elements/graph-d3-force/graph-d3-force.js
@@ -4,7 +4,7 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
 import console from "../../helper/console.js";
 import workerize from "https://cdn.jsdelivr.net/gh/Jamtis/workerize@patch-1/src/index.js";
-import GraphAddon from "../graph-addon/graph-addon.js";
+import { GraphAddon } from "../graph-addon/graph-addon.js";
 import require from "../../helper/require.js";
 import __try from "../../helper/__try.js";
 import requestTimeDifference from "../../helper/requestTimeDifference.js";

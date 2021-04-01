db87842d0a3b4f7f546bb3ceb043f14f6318844b
Nicholas-Philip Brandt <nicholas.brandt@mail.de>
2021-04-01 14:16:25 +0200

    Update graph.extension.js

--- sandbox/graph.extension.js
+++ sandbox/graph.extension.js
@@ -1,3 +1,4 @@
+// Graph extension
 Graph.prototype.addBiEdge = function(a, b, v_0, v_1) {
     this.ensureEdge(a, b, v_0);
     this.ensureEdge(b, a, v_1);
@@ -37,3 +38,50 @@ Graph.prototype.biseparation = function() {
     const first_key = [...this.vertices()][0][0];
     return new Set([[first_key], ...this.complement().verticesWithPathFrom(first_key)].map(([key]) => key));
 }
+
+Graph.prototype.disrupt = function disrupt(set = new Set, disruptors = new Set) {
+    for (const disruptor of disruptors) {
+        for (const party of set) {
+            if (!disruptors.has(party)) {
+                this.spanEdge(party, disruptor);
+            }
+        }
+    }
+};
+
+Graph.create = function create(n) {
+    const g = new Graph;
+    for (let i=0; i<n; ++i) {
+        g.addVertex(i);
+    }
+    return g;
+}
+
+// Array extension
+Array.prototype.iterateSubsets = function*(filter = () => true) {
+    const classes = new Set([new Set]);
+    for (const key of this) {
+        for (const _class of classes) {
+            if (_class.size <= n && !_class.has(key)) {
+                const new_class = new Set([key, ...class]);
+                classes.add(new_class);
+            }
+        }
+    }
+    for (const _class of classes) {
+        if (filter(_class)) {
+            yield _class;
+        }
+    }
+};
+
+// Set extension
+Set.prototype.intersect = function intersect(set) {
+    const intersection = new Set;
+    for (const value of this) {
+        if (set.has(value)) {
+            intersection.add(value);
+        }
+    }
+    return intersection;
+};
